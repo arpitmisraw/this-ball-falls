cf6f0817046ca415a0336cc86a5c9084685c5859
tipra2614 <iit2017088@iiita.ac.in>
2018-01-19 01:47:26 +0530

    Added physics

--- ballfall.js
+++ ballfall.js
@@ -1,24 +1,31 @@
-<<<<<<< HEAD
 var canvas = document.querySelector("canvas");
-canvas.width = window.innerWidth();
-canvas.height = window.innerHeight();
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
-var g = 9.8, e = 0.8;
+var g = 9.8, e = 0, dt = 0.16, x_inp, y_inp;
+
+
+
+c.translate(0, canvas.height);
+c.scale(-1, 1); 
+c.rotate(-Math.PI);
 
 var ball = 
 {
-    x : 0,
-    y : 0,
+    x : canvas.width / 2,
+    y : canvas.height/2,
     u : 0,
     ux : 0,
     uy : 0,
     vx : 0,
     vy : 0,
     theta : 0,
+    color : 'black',
+    radius : 70,
     draw : function()
             {
                 c.beginPath();
-                c.arc(this.x, this.y + this.radius, this.radius, 0, Math.PI*2, true);
+                c.arc(this.x, this.y , this.radius, 0, Math.PI* 2);
                 c.closePath();
                 c.fillStyle = this.color;
                 c.fill();
@@ -27,27 +34,72 @@ var ball =
 
     game : function()
             {
+                clear();
                 this.draw();
-                this.uy -= g*dt;
-                this.y += this.uy*dt;
-                this.x += this.ux * dt;
-                if (this.y  < 0 && t !== 0) 
+                this.vy -= g*dt;
+                this.y += this.vy * dt;
+                this.x += this.vx * dt;
+                if (this.y - this.radius < 0) 
                 {
-                    this.y = 0;
-                    this.uy = e * this.uy;
+                    this.y = this.radius;
+                    this.vy *= -e;
                 }
 
-                if(this.x > canvas.width || this.x <= 0)
+                if(this.x + this.radius > canvas.width || this.x - this.radius <= 0)
                 {
-                    this.ux *= -1;
+                    this.vx *= -1;
                 }
-                
+            },
+    
+    changeDirection : function()
+            {
+                if(Math.sqrt(((this.x - x_inp) * (this.x - x_inp)) + ((this.y - y_inp) * (this.y - y_inp))) < this.radius + 50)
+                {
+                    if(this.vx * (this.x - x_inp) > 0)
+                    {
+                        this.vx += (this.x - x_inp)/2;
+                    }
+                    else
+                    {
+                        this.vx = (this.x - x_inp)/2;
+                    }
+                    // if(this.vy * (this.y - y_inp) > 0)
+                    // {
+                    //     this.vy += (this.y - y_inp)*2;
+                    // }
+                    // else
+                    // {
+                    //     this.vy = (this.y - y_inp)*2;
+                    // }
+                    this.vy = 50;
+                    // this.vy = (this.x - x_inp);
+                }
+                x_inp = 0;
+                y_inp = 0;
             }
+            
 }
-=======
-var canvas = document.querySelector('canvas');
 
-//Setting canvas to take up whole screen
-canvas.width = window.innerWidth();
-canvas.height = window.innerHeight();
->>>>>>> 4294a7e1f38f67c670696dd0e1eab3572af6e848
+
+canvas.addEventListener("click", function()
+{
+    x_inp = event.pageX - canvas.offsetLeft;
+    y_inp = canvas.height - event.pageY - 12;
+    console.log(x_inp, y_inp);
+}
+);
+
+function clear()
+{
+    c.fillStyle = 'white';
+    c.fillRect(0, 2, canvas.width, canvas.height);
+}
+
+function animate()
+{
+    ball.game();
+    ball.changeDirection();
+    requestAnimationFrame(animate);
+}
+
+animate();
